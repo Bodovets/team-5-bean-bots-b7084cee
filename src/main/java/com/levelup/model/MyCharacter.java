4de2f7abf86fc3756e509e1a9a3f2c3596ed0a40
Muralidhar Rachuri <rrmdhar@gmail.com>
2024-02-15 17:49:35 -0500

    Added test case in character for default position

--- src/main/java/com/levelup/model/MyCharacter.java
+++ src/main/java/com/levelup/model/MyCharacter.java
@@ -3,11 +3,14 @@ package com.levelup.model;
 public class MyCharacter {
 
     public String DEFAULT_NAME;
-    
+
     private String name;
+
+    private Position position;
 
     public MyCharacter() {
         DEFAULT_NAME = "BeanBot";
+        position = new Position();
     }
 
     public MyCharacter(String name) {
@@ -20,6 +23,10 @@ public class MyCharacter {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Position getPosition() {
+        return position;
     }
 
 }
